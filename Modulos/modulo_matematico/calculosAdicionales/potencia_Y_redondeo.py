9334fa35c2d707ff187c43a37c3026f79867ee92
Chemssy <jhericho2102@gmail.com>
2026-06-29 18:10:57 -0500

    Modules and Packages

---
+++ Modulos/modulo_matematico/calculosAdicionales/potencia_Y_redondeo.py
@@ -0,0 +1,6 @@
+def potencia(base,exponente):
+    
+    print(f"El resultado de la potencia es :{base**exponente}")
+    
+def redondear(numero):
+    print(f"El numero redondeado es: {round(numero)} ")
---
+++ Modulos/trabajo_con_modulo.py
@@ -0,0 +1,11 @@
+from Modulos.modulo_matematico.calculosBasicos.funciones_matematicas import *
+
+# from modulo_matematico.calculosAdicionales.potencia_Y_redondeo import *
+
+
+multiplicar(3,3)
+
+sumar(10,2)
+restar(30,20)
+
+# potencia(5,3)
